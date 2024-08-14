4188dcfb3278711694d954e93720d20da810843d
USAMI Kenta <tadsan@zonu.me>
2024-08-14 10:26:21 +0900

    Fix incorrect URL to GitHub

--- plz-event-source.el
+++ plz-event-source.el
@@ -4,7 +4,7 @@
 
 ;; Author: r0man <roman@burningswell.com>
 ;; Maintainer: r0man <roman@burningswell.com>
-;; URL: https://github.com/r0man/plz-event-source.el
+;; URL: https://github.com/r0man/plz-event-source
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "26.3") (plz-media-type "0.1.0"))
 ;; Keywords: comm, network, http
